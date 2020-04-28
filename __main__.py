fefb9d8acd2513ed58d0a1a3fd6fb719f0ba494d
Julien Eid <jeid@grubhub.com>
2020-04-28 12:10:15 -0400

    tmp commit

--- __main__.py
+++ __main__.py
@@ -7,24 +7,30 @@ def main():
     service_config = config.require_object("service")
     dict_names = [{"name": d.get("name", None)} for d in service_config["dictionaries"]]
     service = Servicev1(service_config["name"], name=service_config["name"], domains=service_config["domains"],
-                        dictionaries=dict_names, backends=service_config["backends"], activate=False,
+                        dictionaries=dict_names, backends=service_config["backends"], activate=True,
                         force_destroy=True)
-    pulumi.export("Dictionaries", service.dictionaries)
+    print (service.dictionaries)
+    #pulumi.export("Dictionaries", service.dictionaries)
     di = []
     for dictionary in service_config["dictionaries"]:
         print(dictionary['name'])
         tmp_dictionary_id = service.dictionaries.apply(
             lambda x: get_dictionary_id(x, dictionary.get("name")))
-        dictionary_id = pulumi.Output.all(service.id, tmp_dictionary_id).apply(tmp(service.dictionaries))
+        print("a")
+        dictionary_id = pulumi.Output.all(service.id, tmp_dictionary_id, service.dictionaries).apply(tmp)
+        print("b")
         di.append(
             ServiceDictionaryItemsv1(dictionary["name"], pulumi.ResourceOptions(parent=service), service_id=service.id,
                                      items=dictionary["items"],
                                      dictionary_id=dictionary_id))
-        pulumi.export("Dictionary Items", di)
+        print("c")
+        #pulumi.export("Dictionary Items", di)
 
 
 
 def tmp(lst):
+    print("tmp")
+    print(lst)
     print (lst[1])
     return lst[1]
 
@@ -32,6 +38,7 @@ def tmp(lst):
 def get_dictionary_id(dictionaries, dict_name):
     for i in dictionaries:
         if dict_name == i["name"]:
+            print("get_dictionary_id")
             print(i)
             return i.get("dictionary_id")
 
